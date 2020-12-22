e7e49b2c0bd914ba5a0b7c03b4bd36b186bc47ca
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2020-12-22 19:52:40 +0900

    Disable screensaver and lock

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,6 +31,11 @@ sleep 2
 
 export DISPLAY=:0
 mate-session &
+sleep 2
+
+gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 
 echo "Session Running. Press [Return] to exit."
 read
