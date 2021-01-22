7137a953281d58faa50a0921e81df60d94bf0c4c
Restyled.io <commits@restyled.io>
2021-01-22 16:41:34 +0000

    Restyled by shellharden

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,17 @@ set -e
 
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
-echo "user:${VNCPASS}" | sudo chpasswd
+echo "user:$VNCPASS" | sudo chpasswd
 
 # NVIDIA driver version inside the container from Dockerfile must be equal to the host
-HEX_ID=$(sudo nvidia-smi --query-gpu=pci.bus_id --id=$(echo $NVIDIA_VISIBLE_DEVICES | cut -d ',' -f1) --format=csv | sed -n 2p)
-IFS=":." ARR_ID=($HEX_ID)
+HEX_ID=$(sudo nvidia-smi --query-gpu=pci.bus_id --id="$(echo "$NVIDIA_VISIBLE_DEVICES" | cut -d ',' -f1)" --format=csv | sed -n 2p)
+IFS=":." ARR_ID=("$HEX_ID")
 unset IFS
 BUS_ID=PCI:$((16#${ARR_ID[1]})):$((16#${ARR_ID[2]})):$((16#${ARR_ID[3]}))
 # Leave out --use-display-device=None if GPU is headless such as Tesla and download links of such GPU drivers in Dockerfile should also be changed
-sudo nvidia-xconfig --virtual=${SIZEW}x${SIZEH} --allow-empty-initial-configuration --enable-all-gpus --no-use-edid-dpi --busid=$BUS_ID --use-display-device=None
+sudo nvidia-xconfig --virtual="${SIZEW}x$SIZEH" --allow-empty-initial-configuration --enable-all-gpus --no-use-edid-dpi --busid="$BUS_ID" --use-display-device=None
 
-if [ "x${SHARED}" == "xTRUE" ]
+if [ "x$SHARED" == "xTRUE" ]
 then
     export SHARESTRING="-shared"
 fi
@@ -21,30 +21,30 @@ fi
 shopt -s extglob
 for TTY in /dev/tty+([0-9])
 do
-if [ -w $TTY ]
+if [ -w "$TTY" ]
 then
-    /usr/bin/X tty$(echo $TTY | grep -Eo '[0-9]+$') :0 &
+    /usr/bin/X tty"$(echo "$TTY" | grep -Eo '[0-9]+$')" :0 &
     break
 fi
 done
 sleep 1
 
-x11vnc -display :0 -passwd $VNCPASS -forever -xkb -rfbport 5900 $SHARESTRING &
+x11vnc -display :0 -passwd "$VNCPASS" -forever -xkb -rfbport 5900 "$SHARESTRING" &
 sleep 1
 
 /opt/noVNC/utils/launch.sh --vnc localhost:5900 --listen 5901 &
 sleep 1
 
 export DISPLAY=:0
-if vulkaninfo | grep $(echo $NVIDIA_VISIBLE_DEVICES | cut -d ',' -f1 | cut -c 5-) | grep -q ^
+if vulkaninfo | grep "$(echo "$NVIDIA_VISIBLE_DEVICES" | cut -d ',' -f1 | cut -c 5-)" | grep -q ^
 then
 VK=0
 while true
 do
-if ENABLE_DEVICE_CHOOSER_LAYER=1 VULKAN_DEVICE_INDEX=$VK vulkaninfo | grep $(echo $NVIDIA_VISIBLE_DEVICES | cut -d ',' -f1 | cut -c 5-) | grep -q ^
+if ENABLE_DEVICE_CHOOSER_LAYER=1 VULKAN_DEVICE_INDEX=$VK vulkaninfo | grep "$(echo "$NVIDIA_VISIBLE_DEVICES" | cut -d ',' -f1 | cut -c 5-)" | grep -q ^
 then
     export ENABLE_DEVICE_CHOOSER_LAYER=1
-    export VULKAN_DEVICE_INDEX=$VK
+    export VULKAN_DEVICE_INDEX="$VK"
     break
 fi
 VK=$((VK+1))
