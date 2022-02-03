0bd35e6ee5f3ba8aa6934940eff0f5291f6797b7
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2022-02-03 23:12:11 +0900

    noVNC Interface Hotfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -76,8 +76,8 @@ until [ -S "/tmp/.X11-unix/X${DISPLAY/:/}" ]; do sleep 1; done
 echo "X socket is ready"
 
 if [ "$NOVNC_ENABLE" = "true" ]; then
-  if [ -n "$NOVNC_VIEWPASS" ]; then export NOVNC_VIEWONLY="-viewpasswd"; fi
-  sudo x11vnc -display "${DISPLAY}" -passwd "${BASIC_AUTH_PASSWORD:-$PASSWD}" "${NOVNC_VIEWONLY}" "${NOVNC_VIEWPASS}" -shared -forever -repeat -xkb -xrandr "resize" -rfbport 5900 &
+  if [ -n "$NOVNC_VIEWPASS" ]; then export NOVNC_VIEWONLY="-viewpasswd ${NOVNC_VIEWPASS}"; else unset NOVNC_VIEWONLY; fi
+  sudo x11vnc -display "${DISPLAY}" -passwd "${BASIC_AUTH_PASSWORD:-$PASSWD}" -shared -forever -repeat -xkb -xrandr "resize" -rfbport 5900 ${NOVNC_VIEWONLY} &
   /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 8080 --heartbeat 10 &
 fi
 
