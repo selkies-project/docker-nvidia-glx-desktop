68d77fe4c85e64bcd6fb75c1ff80a7a9f03c19fe
ehfd <8457324+ehfd@users.noreply.github.com>
2023-01-19 08:46:25 +0000

    MPLv2 Notice

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Ubuntu release versions 22.04, 20.04, and 18.04 are supported
 ARG UBUNTU_RELEASE=22.04
 ARG CUDA_VERSION=11.7.1
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 # Create and modify permissions of XDG_RUNTIME_DIR
