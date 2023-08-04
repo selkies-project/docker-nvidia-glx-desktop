21b962dfe1870ba409696f1fca653c363f2fc8dc
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2023-08-04 16:35:28 +0900

    Fixes for user-raised issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+trap "echo TRAPed signal" HUP INT QUIT TERM
 
 # Create and modify permissions of XDG_RUNTIME_DIR
 sudo -u user mkdir -pm700 /tmp/runtime-user
