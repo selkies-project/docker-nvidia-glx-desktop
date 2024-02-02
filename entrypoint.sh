d49df24d44bb71ab9642e6bebd4aeffe8ec5b0d8
Justin Bowes <justinb@nvidia.com>
2024-02-02 01:03:49 -0800

    Fix: GPUs on nonzero PCI domain (#51)
    * Fix: GPUs on nonzero PCI domain
    
    Fixes https://github.com/selkies-project/docker-nvidia-glx-desktop/issues/50
    
    ---------
    
    Co-authored-by: Seungmin Kim <8457324+ehfd@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -91,11 +91,11 @@ else
   export CONNECTED_MONITOR="--connected-monitor=${VIDEO_PORT}"
 fi
 
-# Bus ID from nvidia-smi is in hexadecimal format, should be converted to decimal format which Xorg understands, required because nvidia-xconfig doesn't work as intended in a container
+# Bus ID from nvidia-smi is in hexadecimal format and should be converted to decimal format (including the domain) which Xorg understands, required because nvidia-xconfig doesn't work as intended in a container
 HEX_ID="$(sudo nvidia-smi --query-gpu=pci.bus_id --id="$GPU_SELECT" --format=csv | sed -n 2p)"
 IFS=":." ARR_ID=($HEX_ID)
 unset IFS
-BUS_ID="PCI:$((16#${ARR_ID[1]})):$((16#${ARR_ID[2]})):$((16#${ARR_ID[3]}))"
+BUS_ID="PCI:$((16#${ARR_ID[1]}))@$((16#${ARR_ID[0]})):$((16#${ARR_ID[2]})):$((16#${ARR_ID[3]}))"
 # A custom modeline should be generated because there is no monitor to fetch this information normally
 export MODELINE="$(cvt -r "${SIZEW}" "${SIZEH}" "${REFRESH}" | sed -n 2p)"
 # Generate /etc/X11/xorg.conf with nvidia-xconfig
