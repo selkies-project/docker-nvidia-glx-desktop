6bd80b42d66c2fb2cc3b47fe14e448a647c4faa7
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-25 22:56:41 +0900

    Fix KasmVNC

--- selkies-gstreamer-entrypoint.sh
+++ selkies-gstreamer-entrypoint.sh
@@ -64,6 +64,8 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
+        proxy_set_header        X-Real-IP $remote_addr;
+        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -82,6 +84,8 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
+        proxy_set_header        X-Real-IP $remote_addr;
+        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -108,10 +112,14 @@ server {
     }
 
     location /vnc {
+        rewrite ^/vnc(.*)$ $1?$args break;
+
         proxy_set_header        Upgrade \$http_upgrade;
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
+        proxy_set_header        X-Real-IP 127.0.0.1;
+        proxy_set_header        X-Forwarded-For 127.0.0.1;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
