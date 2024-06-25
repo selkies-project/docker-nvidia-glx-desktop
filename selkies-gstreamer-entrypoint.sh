31d8dc7b8f14fcdf43e64bc48c18653db58dd996
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-25 17:53:15 +0900

    Remove remote IP

--- selkies-gstreamer-entrypoint.sh
+++ selkies-gstreamer-entrypoint.sh
@@ -64,8 +64,6 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
-        proxy_set_header        X-Real-IP \$remote_addr;
-        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -84,8 +82,6 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
-        proxy_set_header        X-Real-IP \$remote_addr;
-        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -116,8 +112,6 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
-        proxy_set_header        X-Real-IP \$remote_addr;
-        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
