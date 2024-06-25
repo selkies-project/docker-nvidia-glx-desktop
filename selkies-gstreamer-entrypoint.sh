a8dd53ff14f9471c7e76923d339ec4b1a98500be
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-25 23:00:46 +0900

    Fix typo

--- selkies-gstreamer-entrypoint.sh
+++ selkies-gstreamer-entrypoint.sh
@@ -64,8 +64,8 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
-        proxy_set_header        X-Real-IP $remote_addr;
-        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header        X-Real-IP \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -84,8 +84,8 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
-        proxy_set_header        X-Real-IP $remote_addr;
-        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header        X-Real-IP \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -112,7 +112,7 @@ server {
     }
 
     location /vnc {
-        rewrite ^/vnc(.*)$ $1?$args break;
+        rewrite ^/vnc(.*)\$ \$1?\$args break;
 
         proxy_set_header        Upgrade \$http_upgrade;
         proxy_set_header        Connection \"upgrade\";
